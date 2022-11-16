031f3c3aab0aa6c7491e680b193abfa561287591
Oscar Busk <oscar.busk@gmail.com>
2022-11-16 02:10:45 +0100

    Add co-located component (with name that works)
    Following https://beta.nextjs.org/docs/routing/defining-routes#catch-all-and-optional-catch-all-segments

---
+++ app/[...slug]/moon.tsx
@@ -0,0 +1,3 @@
+export default function ThisIsNotApage() {
+  return <h1>This is not a page</h1>;
+}
--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -1,7 +1,14 @@
+import ThisIsNotApage from "./moon";
+
 export default function SlugPage({
   params: { slug },
 }: {
   params: { slug: string[] };
 }) {
-  return <h1>Slug: {slug.join("/")}</h1>;
+  return (
+    <>
+      <h1>Slug: {slug.join("/")}</h1>
+      <ThisIsNotApage />
+    </>
+  );
 }
